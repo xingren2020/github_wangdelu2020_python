98a16ff1511342ff274a5e3c214c1d2939ecf63c
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-11-16 07:21:44 +0800

    Update chicken.py

--- airplay/chicken.py
+++ airplay/chicken.py
@@ -6,21 +6,17 @@ import re
 import urllib
 from datetime import datetime
 from dateutil import tz
-
+#by 红鲤鱼绿鲤鱼与驴 2020.11
 mins = int(time.time())
 date_stamp = (mins-57600) % 86400
 print(datetime.now(tz=tz.gettz('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S", ))
 _datatime = datetime.now(tz=tz.gettz('Asia/Shanghai')).strftime("%Y%m%d", )
 start=datetime.now().minute%10
-
-
 result=''
 djj_bark_cookie=''
 djj_sever_jiang=''
 airplay_count_cookie=''
-
 cookiesList=[]
-
 def airplay_sign(ck):
     headers = {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1','Host':'glados.rocks','Cookie':ck}
@@ -32,18 +28,20 @@ def airplay_sign(ck):
     msg=f"""打卡成功✅{obj['message']}打卡时间:{obj['list'][0]['detail']}剩余{x[0]}天"""
     loger(msg)
     
-def check():
+def checkio():
    print('Localtime',datetime.now(tz=tz.gettz('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S", ))
-   global airplay_count_cookie
    global djj_bark_cookie
    global djj_sever_jiang
    if "DJJ_BARK_COOKIE" in os.environ:
      djj_bark_cookie = os.environ["DJJ_BARK_COOKIE"]
-   if "AIRPLAY_COUNT_COOKIE" in os.environ:
-      airplay_count_cookie = os.environ["AIRPLAY_COUNT_COOKIE"]
+
    if "DJJ_SEVER_JIANG" in os.environ:
       djj_sever_jiang = os.environ["DJJ_SEVER_JIANG"]
       
+def checklocal():
+   global airplay_count_cookie
+   if "AIRPLAY_COUNT_COOKIE" in os.environ:
+      airplay_count_cookie = os.environ["AIRPLAY_COUNT_COOKIE"]
       for line in airplay_count_cookie.split('\n'):
         if not line:
           continue 
@@ -80,8 +78,8 @@ def loger(m):
    global result
    result +=m+'\n'
 def start():
-   check()
-   #print(cookiesList)
+   checkio()
+   checklocal()
    j=0
    for count in cookiesList:
      j+=1
