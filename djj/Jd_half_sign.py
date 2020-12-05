45cc9250c4c59e4e52432534097091e337c3b3aa
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-12-05 10:52:28 +0800

    Update Jd_half_sign.py

--- djj/Jd_half_sign.py
+++ djj/Jd_half_sign.py
@@ -378,8 +378,8 @@ def check(flag,list):
      djj_bark_cookie = os.environ["DJJ_BARK_COOKIE"]
    if "DJJ_SEVER_JIANG" in os.environ:
       djj_sever_jiang = os.environ["DJJ_SEVER_JIANG"]
-   if flag in osenviron:
-      vip = osenviron[flag]
+   if flag in os.environ:
+      vip = os.environ[flag]
    if vip:
        for line in vip.split('\n'):
          if not line:
