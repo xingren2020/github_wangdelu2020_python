a51f24788f0cb13872daaf6575506f2e24c9863e
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-12-09 10:12:45 +0800

    Update Sam_youku.py

--- Sam/Sam_youku.py
+++ Sam/Sam_youku.py
@@ -2,23 +2,28 @@ import requests
 import os
 import re
 import json
+import random
+import time
 osenviron={}
 
-
+urllist=[]
+hdlist=[]
+bdlist=[]
 
 def Av(i,hd,j,k):
     print('=🔔='*k)
     try:
+      
        response = requests.post(i,headers=eval(hd),data=j)
        Res=response.json()
-       
+       #print(Res)
        if(k==1):
            print(Res['msg'])
        elif(k==2):
          print(Res['msg'])
          for item in Res['data']['task_list']:
            if(json.dumps(item).find('get_coin')>0):
-             #print('find')
+             
              if(item['status']==2):
                Av(urllist[2],hdlist[0],bdlist[2],3)
        elif(k==3):
@@ -50,19 +55,27 @@ def watch(flag,list):
 
 
 def start():
+   global bdlist,urllist,hdlist
    urllist=[]
    hdlist=[]
    bdlist=[]
+   time.sleep(random.randint(1,5))
    watch('sam_url',urllist)
    watch('sam_headers',hdlist)
-   watch('sam_body',bdlist)
-   for j in range(1):
+   for j in range(10):
        print('====count===='+str(j))
+       watch('sam_body'+str(j),bdlist)
+       if(len(bdlist)==0):
+            break
        for k in range(len(urllist)):
           if(k==2):
              continue
           Av(urllist[k],hdlist[0],bdlist[k],(k+1))
+       time.sleep(random.randint(1,3))
    print('🔔'*15)
+   
+   
+
 if __name__ == '__main__':
        start()
     
