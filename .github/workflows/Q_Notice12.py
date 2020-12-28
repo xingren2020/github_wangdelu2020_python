ad56f99db8c7694af8ba361130038a5ba6406562
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-12-28 11:04:14 +0800

    Create Q_Notice12.py

---
+++ .github/workflows/Q_Notice12.py
@@ -0,0 +1,30 @@
+name: Q_Notice12
+
+on:
+  schedule:
+    - cron: '*/20 * * * * '
+  watch:
+    types: [started]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+        
+      - name: 'Set up Python'
+        uses: actions/setup-python@v1
+        with:
+          python-version: 3.7
+         
+      - name: 'Install requirements'
+        run: pip install -r ./Q_sub/requirements.txt 
+        
+      - name: '公众号iosrule' 
+        run: python3 ./Q_sub/Q_Notice12.py
+        env:
+            DJJ_BARK_COOKIE: ${{ secrets.DJJ_BARK_COOKIE }}
+            DJJ_SEVER_JIANG: ${{ secrets.DJJ_SEVER_JIANG }}
+            ios_wturl: ${{ secrets.ios_newurl }}
+            ios_newhd: ${{ secrets.ios_newhd }}
+            ios_newbt: ${{ secrets.ios_newbt }}
