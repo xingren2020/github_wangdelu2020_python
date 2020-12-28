72a88204a0065dbd7b9a8dfa87db39ce70ad78ef
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-12-28 11:05:03 +0800

    Update Q_main12.py

--- Q_sub/Q_main12.py
+++ Q_sub/Q_main12.py
@@ -170,7 +170,7 @@ def start():
      if mm<2:
        time.sleep(600)
    #if notice('4:00','5:00') or notice('22:00','23:00') or notice('13:00','14:00'):
-   pushmsg('公众号iosrule',result)
+   #pushmsg('公众号iosrule',result)
      
      
     
